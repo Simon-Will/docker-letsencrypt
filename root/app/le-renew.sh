6bbbfae5573862f74ff10d907dbeeaf8b838af24
Simon Will <will@cl.uni-heidelberg.de>
2017-08-23 09:30:20 +0200

    Combine privkey.pem and fullchain.pem for HAProxy

--- root/app/le-renew.sh
+++ root/app/le-renew.sh
@@ -5,4 +5,4 @@ echo
 echo "<------------------------------------------------->"
 echo "cronjob running on "$(date)
 echo "Running certbot renew"
-certbot -n renew --standalone --pre-hook "s6-svc -d /var/run/s6/services/nginx" --post-hook "s6-svc -u /var/run/s6/services/nginx ; cd /config/keys/letsencrypt && openssl pkcs12 -export -out privkey.pfx -inkey privkey.pem -in cert.pem -certfile chain.pem -passout pass:"
+certbot -n renew --standalone --pre-hook "s6-svc -d /var/run/s6/services/nginx" --post-hook "s6-svc -u /var/run/s6/services/nginx ; cd /config/keys/letsencrypt && openssl pkcs12 -export -out privkey.pfx -inkey privkey.pem -in cert.pem -certfile chain.pem -passout pass: ; cd /config/keys/letsencrypt && mkdir -p /haproxy_certs && cat privkey.pem fullchain.pem > /haproxy_certs/cert1.pem"
